fc2b6a91b7d20432aa1a3f3cde63f14956821e01
melkwastaken <77582036+melkwastaken@users.noreply.github.com>
2021-03-05 12:19:29 +0000

    beeg brain v2.2

---
+++ stable.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Stable Thing");  
+      Console.WriteLine("Unstable To Stable");
+     {
+     static void Main(string[morespaces] args)  
+    }
+  }
+}
